aeeaa44652892ed645f4f5a57e7c87c34eae8a07
Adhamadi <adhamadimojo@gmail.com>
2017-10-18 16:04:02 +0700

    mengganti controller

--- application/controller/Login.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit ('No direct script access allowed');
-class Login extends CI_Controller {
-	public function index() {
-		$data = array('title' => 'Login Administrator',
-		‘isi’ =>’admin/login_view’);
-		$this->load->view(‘admin/login_view’,$data);
-	}
-}
