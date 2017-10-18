21c738d5d3c867bb733ec33bdc3cd1a86cad9430
OmanNewbie <rochman_darmawan@yahoo.com>
2017-10-18 16:15:58 +0700

    layout

---
+++ application/views/layout/footer.php
@@ -0,0 +1,4 @@
+ <div class="clearfix"></div>
+    <footer><a href="http://is.its.ac.id" target="_blank">Konstruksi dan Pengujian Perangkat Lunak D</a> | &copy;Peminjaman Sepeda - 2017</footer></div>
+</body>
+</html>
---
+++ application/views/layout/header.php
@@ -0,0 +1,16 @@
+<header>
+    	<div id="logo"><a href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>assets/images/bicycle.png" width="400" height="400"></a></div>
+        <div id="nama"><span class="nama">PEMINJAMAN SEPEDA</span><br>
+        <span class="aipni">Selamat Datang di Sistem Informasi Peminjaman Sepeda</span></div>
+  </header>
+
+<nav>
+  	<ul>
+    	<li><a href="<?php echo base_url(); ?>">Home</a></li>
+        <li><a href="<?php echo base_url(); ?>admin/About">About Us</a></li>
+        <li><a href="<?php echo base_url(); ?>admin/login">Login</a></li>
+        <li><a href="<?php echo base_url(); ?>admin/Kontak">Kontak</a></li>
+        <li><a href="http://is.its.ac.id" target="_blank">Website Pendukung</a></li>
+
+    </ul>
+  </nav>
